1701c2044c43153586d6310e545ca2007bfdd15b
ranierigalasso <ranierigalasso@gmail.com>
2019-01-17 18:26:32 +0100

    created 3 screens and some css

--- js/main.js
+++ js/main.js
@@ -1,2 +1,18 @@
 "use strict";
 
+
+//Load Screens & Game (STATES TRANSITION)
+function loadSplashScreen () {
+
+}
+function loadGameScreen () {
+
+}
+function loadGameOverScreen () {
+
+}
+function loadGame () {
+
+}
+
+window.addEventListener("load",loadGame);
